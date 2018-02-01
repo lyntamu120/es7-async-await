f470ce2020dbcafcc64a60060f2e03e503fd5b53
王希宁 <xiningwang120@gmail.com>
2018-02-01 16:41:20 -0600

    Add Error handlings

--- currency-convert.js
+++ currency-convert.js
@@ -3,16 +3,29 @@
 
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-    return axios.get(`https://api.fixer.io/latest?base=${from}`).then((response) => {
-        return response.data.rates[to];
-    });
+const getExchangeRate = async (from, to) => {
+    try {
+        const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
+        const rate = response.data.rates[to];
+
+        if (rate) {
+            return rate;
+        } else {
+            throw new Error();
+        }
+    } catch (e) {
+        throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+    }
+
 };
 
-const getCountries = (currencyCode) => {
-    return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then(response => {
+const getCountries = async (currencyCode) => {
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
         return response.data.map(country => country.name);
-    });
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}`);
+    }
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -33,5 +46,8 @@ const convertCurrencyAlt = async (from, to, amount) => {
     return `${amount} ${from} is worth ${exchangedAmount} ${to}. You can spend these in the following countries: ${countries.join(', ')}`;
 };
 
-// convertCurrency('USD', 'CAD', 100).then(status => console.log(status));
-convertCurrencyAlt('USD', 'CAD', 100).then(status =>console.log(status));
+convertCurrencyAlt('USD', 'CAD', 100).then(status => {
+    console.log(status)
+}).catch(e => {
+    console.log(e.message);
+});
