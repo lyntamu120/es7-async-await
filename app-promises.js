89eac0e357767526f2f1b23fd669b4fb66e5523c
王希宁 <xiningwang120@gmail.com>
2018-02-01 16:07:28 -0600

    Add getStatusAlt using async-await

--- app-promises.js
+++ app-promises.js
@@ -62,11 +62,23 @@ const getStatus = (userId) => {
 //         return resolve('Mike');
 //     });
 // }
+
 const getStatusAlt = async (userId) => {
-    return 'Mike';
+    const user = await getUser(userId);
+    const grades = await getGrades(user.schoolId);
+    let average = 0;
+
+    if (grades.length > 0) {
+        average = grades.map(grade => grade.grade).reduce((a, b) => a + b) / grades.length;
+    }
+
+    return `${user.name} has a ${average} in the class`
 };
 
-console.log(getStatusAlt());
+getStatusAlt(1)
+    .then(status => console.log(status))
+    .catch(e => console.log(e));
+
 
 // getStatus(1).then((status) => {
 //     console.log(status);
